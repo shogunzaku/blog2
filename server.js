3180efe647c9919d75243b8ae322112d6e1c477e
Shogun <shogunzaku@gmail.com>
2022-09-06 09:49:23 -0700

    moved index.ejs into articles folder

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     createdAt: new Date(),
     description: 'test description 2'
   }]
-  res.render('index', { articles: articles })
+  res.render('articles/index', { articles: articles })
 })
 
 app.listen(5000)
