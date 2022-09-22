b89a81a2151347c970899b6278ba6fc7d72dcd5e
Shogun <shogunzaku@gmail.com>
2022-09-22 16:26:05 -0700

    debugged mongo db problem

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog'), {
-useNewUrlParser: true}
+mongoose.connect('mongodb://localhost/blog2', { 
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
-app.use('/articles', articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   const articles = [{
@@ -23,5 +24,7 @@ app.get('/', (req, res) => {
   }]
   res.render('articles/index', { articles: articles })
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
