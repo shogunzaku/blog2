6b0a026f334be774e405535de300c03ee042fb4c
Shogun <shogunzaku@gmail.com>
2022-10-04 15:24:57 -0700

    articles now are saved

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
@@ -11,17 +12,8 @@ app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  const articles = [{
-    title: 'Test Article',
-    createdAt: new Date(),
-    description: 'test description'
-  },
-  {
-    title: 'Test Article 2',
-    createdAt: new Date(),
-    description: 'test description 2'
-  }]
+app.get('/', async (req, res) => {
+  const articles = await Article.find()
   res.render('articles/index', { articles: articles })
 })
 
